8d8ce4eadb358620e9ab1a3acc4d1ecffacc2706
Yerson Ramos <vladimirramossullon@gmail.com>
2024-11-06 00:29:14 -0500

    subiendo proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './Components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+
+}
+
+export default App
